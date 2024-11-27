1d027c18801c7497f5935f143ccd010d9b552271
Fiona <fiona.rio@outlook.com>
2024-11-27 15:02:59 +0100

    ajout de playwright et debut de responsive pour navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -5,18 +5,18 @@ import logoCROM from '/src/assets/img/logo_CROM_white.png';
 
 const Navbar = () => {
     return (
-        <nav className="text-white bg-neutral-950 font-bold md:text-xs lg:text-lg uppercase grid grid-flow-col items-center py-6 px-14">
+        <nav className="text-white bg-neutral-950 font-bold  md:text-xs lg:text-lg uppercase grid grid-flow-col items-center sm:py-2 sm:px-2 py-6 px-14">
             <a href="/" className="">
-                <img src={logoBuffle} alt="logo buffle" className="md:max-h-12 lg:max-h-24 hover:animate-spin" />
+                <img src={logoBuffle} alt="logo buffle" className="sm:max-h-16 md:max-h-12 lg:max-h-24" />
             </a>
-            <div className="grid grid-cols-4 justify-items-center">
+            <div className="grid md:grid-cols-4 lg:grid-cols-4 justify-items-center">
                 <a href="/tremplin" className="hover:underline underline-offset-4">#ALM-le tremplin</a>
                 <a href="/about" className="hover:underline underline-offset-4">évenements</a>
                 <a href="/association" className="hover:underline underline-offset-4">l'association</a>
                 <a href="/partenaires" className="hover:underline underline-offset-4">nos partenaires</a>
             </div>
             <a href="/" className="place-items-end">
-                <img src={logoCROM} alt="logo CROM" className="md:max-h-6 lg:max-h-16 pr-6" />
+                <img src={logoCROM} alt="logo CROM" className="sm:max-h-8 md:max-h-6 lg:max-h-16 pr-6" />
             </a>
         </nav>
     );
